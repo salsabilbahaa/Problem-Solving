8ca3a200bf477b97c742e9998e17417feafcb41f
Salsabil Bahaa Eldin <152950734+salsabilbahaa@users.noreply.github.com>
2026-04-17 13:14:20 +0200

    Time: 400 ms (89.66%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
 DELETE p1
-FROM Person AS p1, Person AS p2
-WHERE p1.email = p2.email AND p1.id > p2.id
+FROM Person AS p1
+INNER JOIN Person AS p2
+ON p1.email = p2.email AND p1.id > p2.id
