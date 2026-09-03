71b026c89035a529255ba952a398a87fc93c27c9
Salsabil Bahaa Eldin <152950734+salsabilbahaa@users.noreply.github.com>
2026-09-03 16:28:07 +0300

    Time: 0 ms (100%), Space: 24.2 MB (43.89%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int planted = 0, lastOne = -1, firstOne = -1;
+        int planted = 0;
         for(int i = 0; i < flowerbed.size(); i++){
-            if(flowerbed[i] == 1){
-                if (firstOne == -1){
-                    firstOne = i;
-                    planted += firstOne/2;
-                }
-                else{
-                    planted += (i - lastOne - 2) / 2;
-                }
-                lastOne = i;
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size()-1 || flowerbed[i+1] == 0)){
+                planted++;
+                flowerbed[i] = 1;
             }
-        }
-        if (lastOne == -1){
-            planted += (flowerbed.size() - lastOne) / 2;
-        }
-        else {
-            planted += (flowerbed.size() - lastOne - 1) / 2;
         }
         return planted >= n;
     }
