1b3b684d492ff545bbf001500e3e3f0ddb2b7383
Salsabil Bahaa Eldin <152950734+salsabilbahaa@users.noreply.github.com>
2026-09-03 20:58:59 +0300

    Time: 4 ms (65.18%), Space: 165.9 MB (31.94%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        sort(nums1.begin(), nums1.end());
-        if (nums1[0] & 1){
+        int mn = 1e9;
+        for (int i : nums1){
+             mn = min(mn, i);
+        }
+        if (mn & 1){
             return true;
         }
         bool can = 1;
